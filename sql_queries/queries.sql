c7733a77d6f58869548a87ee62d727f199bb9d11
Dan07619 <60045242+Dan07619@users.noreply.github.com>
2024-06-06 19:18:42 -0600

    Update queries.sql

--- sql_queries/queries.sql
+++ sql_queries/queries.sql
@@ -1,1 +1,27 @@
 -- SQL Queries 
+DATABASE1
+
+SELECT
+    DOC.DNUM AS "# de ticket",
+    DOC.DFECHA AS "Fecha",
+    DOC.DCANT AS "Monto sin IVA",
+    (DOC.DCANT + DOC.DIVA) AS "Monto con IVA",
+    DOC.DPAR1 AS "Vendedor",
+    CLI.CLINOM AS "Cliente",
+    AUX.ICOD AS "Productos comprados (SKU)",
+    INV.IDESCR AS "Descripción",
+    AUX.AIPZAS AS "Cantidad (unidades)",
+    AUX.AIALMACEN AS "Almacén que vendió",
+    INV.ILISTA3 AS "Precio de lista",
+    INV.IFAM3 AS "Talla",
+    INV.IFAM4 AS "Color",
+    INV.IFAM5 AS "Temporada"
+FROM
+    DOCUMENTOS DOC
+    JOIN CLIENTES CLI ON DOC.CLICOD = CLI.CLICOD
+    JOIN AUXILIAR_INVENTARIOS AUX ON DOC.DNUM = AUX.FMOV
+    JOIN INVENTARIOS INV ON AUX.ICOD = INV.ICOD
+WHERE
+    DOC.DESCXC = 1 -- Ventas
+ORDER BY
+    DOC.DNUM;
